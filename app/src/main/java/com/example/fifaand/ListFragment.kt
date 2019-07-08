371e2bad248ce6eff685095a84affeb212c92929
Kacper Błażejewski <kacper.blazejewski@apatro.com>
2019-07-08 16:00:54 +0200

    Phase 2

--- app/src/main/java/com/example/fifaand/ListFragment.kt
+++ app/src/main/java/com/example/fifaand/ListFragment.kt
@@ -6,16 +6,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.fragment_list.*
+import kotlinx.android.synthetic.main.fragment_list.view.*
 
-class ListFragment : Fragment() {
+class ListFragment : Fragment(), View.OnClickListener {
+    private lateinit var dialog: AlertDialog
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+        dialog = AlertDialog.Builder(context!!)
+            .setTitle("Close App")
+            .setMessage("Are you sure you want to quit the app?")
+            .setPositiveButton("YES") { p0, p1 -> activity?.finish() }
+            .setNegativeButton("Cancel") { p0, p1 -> }
+            .create()
+        view.playerListBtn.setOnClickListener(this)
+        view.loadPlayersBtn.setOnClickListener(this)
+        view.exitBtn.setOnClickListener(this)
+
+        return view
     }
 
-
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.playerListBtn -> Toast.makeText(context, "PlayerList", Toast.LENGTH_SHORT).show()
+            R.id.loadPlayersBtn -> Toast.makeText(context, "Load Players", Toast.LENGTH_SHORT).show()
+            R.id.exitBtn -> dialog.show()
+        }
+    }
 }
